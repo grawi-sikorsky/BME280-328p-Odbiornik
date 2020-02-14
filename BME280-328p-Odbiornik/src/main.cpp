25ad84894d7795955346a59440d3c64214ab9eec
Abdul <grawires@gmail.com>
2020-02-14 17:08:43 +0100

    aj

---
+++ BME280-328p-Odbiornik/src/main.cpp
@@ -0,0 +1,34 @@
+#include <VirtualWire.h>
+
+const int receive_pin = 12;
+char temperatureChar[10];
+char humidityChar[10];
+struct package
+{
+  float temperature = 0.0;
+  float humidity = 0.0;
+};
+typedef struct package Package;
+Package data;
+void setup()
+{
+    delay(1000);
+    // Initialise the IO and ISR
+    vw_set_rx_pin(receive_pin);
+    vw_setup(500);   // Bits per sec
+    vw_rx_start();       // Start the receiver PLL running
+}
+void loop()
+{
+    uint8_t buf[sizeof(data)];
+    uint8_t buflen = sizeof(data);
+if (vw_have_message())  // Is there a packet for us? 
+  {
+    vw_get_message(buf, &buflen);
+    memcpy(&data,&buf,buflen);
+    Serial.print("\nPackage:");
+    Serial.print(data.temperature);
+    Serial.print("\n");
+    Serial.println(data.humidity);
+  }
+}

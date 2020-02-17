8729d6b962cedb52172bb8c47ccd82f82d867b71
Abdul <grawires@gmail.com>
2020-02-17 12:56:43 +0100

    1

--- BME280-328p-Odbiornik/src/main.cpp
+++ BME280-328p-Odbiornik/src/main.cpp
@@ -1,10 +1,15 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
 
-RH_ASK rf_receiver;
+#include <avr/power.h>
+
+//RH_ASK rf_receiver;
+RH_ASK rf_receiver(2000,11,12,3,true);
 
 void setup()
 {
+  //clock_prescale_set(clock_div_1);
+  pinMode(13,OUTPUT);
     Serial.begin(9600);	// Debugging only
     if (!rf_receiver.init())
     {
@@ -18,13 +23,15 @@ void setup()
 
 void loop()
 {
-    uint8_t buf[12];
+    uint8_t buf[11];
     uint8_t buflen = sizeof(buf);
     if (rf_receiver.recv(buf, &buflen)) // Non-blocking
     {
-      int i;
       // Message with a good checksum received, dump it.
+      digitalWrite(13,HIGH);
       Serial.print("Message: ");
-      Serial.println((char*)buf);         
+      Serial.println((char*)buf);
+      delay(20);
+      digitalWrite(13,LOW);
     }
 }

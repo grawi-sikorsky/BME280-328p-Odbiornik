1616ea365a2056e2c4ac061c96f03b382a3ba85c
printo3d <grawires@gmail.com>
2020-02-18 18:55:48 +0100

    home

--- BME280-328p-Odbiornik/src/main.cpp
+++ BME280-328p-Odbiornik/src/main.cpp
@@ -1,5 +1,13 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
+#include <Adafruit_INA219.h>
+
+Adafruit_INA219 ina219;
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
 
 RH_ASK rf_receiver;
 
@@ -14,6 +22,15 @@ void setup()
     {
       Serial.println("init ok");
     }
+
+    // Initialize the INA219.
+    // By default the initialization will use the largest range (32V, 2A).  However
+    // you can call a setCalibration function to change this range (see comments).
+    ina219.begin();
+    // To use a slightly lower 32V, 1A range (higher precision on amps):
+    //ina219.setCalibration_32V_1A();
+    // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+    //ina219.setCalibration_16V_400mA();
 }
 
 void loop()
@@ -27,4 +44,18 @@ void loop()
       Serial.print("Message: ");
       Serial.println((char*)buf);         
     }
+
+
+    shuntvoltage = ina219.getShuntVoltage_mV();
+    busvoltage = ina219.getBusVoltage_V();
+    current_mA = ina219.getCurrent_mA();
+    power_mW = ina219.getPower_mW();
+    loadvoltage = busvoltage + (shuntvoltage / 1000);
+    Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+    Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+    Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+    Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+    Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+    Serial.println("");
+    delay(500);
 }
